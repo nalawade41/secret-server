c4570d0e00e11da235b476f52da1189d8bea2818
Mahesh <nalawade41@gmail.com>
2024-08-09 09:40:27 +0530

    Feature/Setup
    Added intital go mod initilization. Added basic logger. Added Basic config for HTTP Server

---
+++ config/config.go
@@ -0,0 +1,56 @@
+package config
+
+import (
+	"github.com/nalawade41/secret-server/internal/util/logger"
+	"os"
+	"path/filepath"
+	"runtime"
+
+	"github.com/joho/godotenv"
+)
+
+const (
+	EnvLocal = "local"
+	Prod     = "production"
+)
+
+var (
+	// Get current file full path from runtime
+	_, b, _, _ = runtime.Caller(0)
+
+	// ProjectRootPath Root folder of this project
+	ProjectRootPath = filepath.Join(filepath.Dir(b), "../")
+)
+
+type (
+	Config struct {
+		Environment string
+		HTTP        *HttpConfig
+	}
+)
+
+// Init populates Config struct with values from config file
+// located at filepath and environment variables.
+func Init() (*Config, error) {
+	// Get the environment
+	env := os.Getenv("APP_ENV")
+
+	// Load .env only if APP_ENV is "local"
+	if env == EnvLocal {
+		// Load environment-specific .env file if it exists
+		envFilePath := ProjectRootPath + "/.env." + env
+		if _, err := os.Stat(envFilePath); err == nil {
+			if err := godotenv.Load(envFilePath); err != nil {
+				logger.Errorf("Error loading enviornment %s file", envFilePath)
+				return nil, err
+			}
+		}
+	}
+
+	http := LoadHttpConfig()
+	config := &Config{
+		Environment: env,
+		HTTP:        http,
+	}
+	return config, nil
+}
